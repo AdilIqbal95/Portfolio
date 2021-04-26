644915ae67a9440c2433f50e357ac9832e37cbcf
“AdilIqbal95” <adil.iqbal@hotmail.co.uk>
2021-04-26 03:01:45 +0100

    added scrollintoview for contact tab

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,12 @@
 let aboutLink = document.querySelector('#aboutLink');
 let skillsLink = document.querySelector('#skillsLink');
 let projectsLink = document.querySelector('#projectsLink');
+let contactsLink = document.querySelector('#contactLink');
 
 let about = document.querySelector('#About_me');
 let Skills = document.querySelector('#Skills');
 let Projects = document.querySelector('#Projects');
+let Contact = document.querySelector('#Contact');
 
 aboutLink.addEventListener('click', function () {
   about.scrollIntoView(true);
@@ -16,6 +18,10 @@ skillsLink.addEventListener('click', function () {
 
 projectsLink.addEventListener('click', function () {
   Projects.scrollIntoView(true);
+});
+
+contactsLink.addEventListener('click', function () {
+  Contact.scrollIntoView(true);
 });
 
 $(".slider").slick({
